da791171bb2f555e5600cfdbbef4c74be4d4324a
Arnon Puitrakul <peter_arnon1@hotmail.com>
2017-12-24 13:22:34 +0700

    :sparkles: Add Pi'Status Showing in the first page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import PiStatus from './components/PiStatus'
+
 import firebase from './utils/firebase'
 import './font.css'
 
-const Container = styled.div`
+const SuperWrapper = styled.div`
+  display: flex;
+  flex-direction:column;
   width: 80%;
   margin: 0 auto;
   margin-top: 80px;
+`
+
+const Contianer = styled.div`
+  display: flex;
+  flex-direction:column;
+`
+
+const Header = styled.h1`
+  color: #424242;
+  margin-top: 0;
 `
 class App extends React.Component {
 
@@ -25,7 +39,6 @@ class App extends React.Component {
 
     dataRef.on('value', (snapshot) => {
       let items = snapshot.val()
-      console.log(items)
       this.setState({
         controls : items.Controls,
         pi: items.PI,
@@ -35,11 +48,16 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state)
     return (
-      <Container>
-        Hello World
-      </Container>
+      <SuperWrapper>
+        <Header>Smart Home Controller</Header>
+        {
+          this.state.controls !== null && this.state.pi !== null && this.state.settings !== null ?
+          <Contianer>
+            <PiStatus pi={this.state.pi}/>
+          </Contianer> : <p>Gathering information from Firebase</p> //Prevent Exception from feeding null value when loading the data
+        }
+      </SuperWrapper>
     );
   }
 }
---
+++ src/components/PiStatus.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Header = styled.h2`
+  color: #424242;
+`
+
+const StatWrapper = styled.div`
+  display: flex;
+  flex-direction:row;
+  flex-wrap:wrap;
+`
+
+const StatHeader = styled.h3`
+  color: #424242;
+  margin: 0;
+`
+const StatInfo = styled.p`
+  color: #424242;
+  margin-bottom: 0
+
+  :first-child {
+    margin-top: 0;
+  }
+`
+
+const StatContainer = styled.div`
+  display: flex;
+  flex-direction:column;
+  flex-wrap:nowrap;
+  border: 2px solid #E0E0E0;
+  padding: 10px 10px 0 10px;
+  margin: 5px 5px 5px 5px;
+  flex-basis: 30%
+
+`
+
+export default class PiStatus extends React.Component {
+  render () {
+    console.log(this.props.pi)
+    return (
+      <Container>
+        <Header>Pi's Status</Header>
+        <StatWrapper>
+
+          <StatContainer>
+            <StatHeader>CPU</StatHeader>
+            <StatInfo>Temperature : {this.props.pi.CPU.temperature}</StatInfo>
+          </StatContainer>
+
+          <StatContainer>
+            <StatHeader>DISK</StatHeader>
+            <StatInfo>Total : {this.props.pi.DISK.total} GB</StatInfo>
+            <StatInfo>Used : {this.props.pi.DISK.used} GB</StatInfo>
+            <StatInfo>Free : {this.props.pi.DISK.free} GB</StatInfo>
+          </StatContainer>
+
+          <StatContainer>
+            <StatHeader>RAM</StatHeader>
+            <StatInfo>Total : {this.props.pi.RAM.total} MB</StatInfo>
+            <StatInfo>Used : {this.props.pi.RAM.used} MB</StatInfo>
+            <StatInfo>Free : {this.props.pi.RAM.free} MB</StatInfo>
+          </StatContainer>
+        </StatWrapper>
+      </Container>
+    )
+  }
+}
